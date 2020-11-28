35379f68e1a10488ad13e45020e399fd090eab65
Kerwin <1078933746@qq.com>
2020-11-28 14:48:32 +0800

    完成登陆页面的搭建

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,24 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Login from "../components/Login.vue";
+import Home from "../components/Home.vue";
 
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+Vue.use(VueRouter);
 
 const router = new VueRouter({
-  routes
+  routes: [
+    { path: "/", redirect: "login" },
+    { path: "/login", component: Login },
+    { path: "/home", component: Home },
+  ],
+});
+
+router.beforeEach((to,from,next) => {
+  //直接放行
+  if(to.path == "/login" ) return next()
+  const tokenStr = window.sessionStorage.getItem('token')
+  if(!tokenStr) return next('/login')
+  next()
 })
 
-export default router
+export default router;
