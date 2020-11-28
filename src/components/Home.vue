50b0c768ceafd83f34d03427c20e92577b874bf5
Kerwin <1078933746@qq.com>
2020-11-28 16:58:44 +0800

    搭建基本框架

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,124 @@
 <template>
-    <div>
-       <el-button type="primary" @click="logout" >退出</el-button>
-    </div>
+  <el-container class="home-container">
+    <!-- 头部 -->
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">
+        <!-- 侧边栏菜单 -->
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          unique-opened
+          border="none"
+        >
+          <!-- 一级菜单 -->
+          <el-submenu
+            :index="item.id + ''"
+            v-for="item in menuList"
+            :key="item.id"
+          >
+            <!-- 一级菜单模板 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconsObj[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{ item.authName }}</span>
+            </template>
+            <!-- 二级子菜单 -->
+            <el-menu-item
+              index="subItem + '' "
+              v-for="subItem in item.children"
+              :key="subItem.id"
+            >
+              <!-- 二级菜单模板 -->
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-menu"></i>
+                <!-- 文本 -->
+                <span>{{ subItem.authName }}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧主体 -->
+      <el-main>Main </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-    methods: {
-        logout() {
-            window.sessionStorage.removeItem('token')
-            this.$router.push('./login')
-        }
+  data() {
+    return {
+      menuList: null,
+      iconsObj: {
+        125: "iconfont icon-user",
+        103: "iconfont icon-tijikongjian",
+        101: "iconfont icon-shangpin",
+        102: "iconfont icon-danju",
+        145: "iconfont icon-baobiao",
+      },
+    };
+  },
+  created() {
+    this.getMenuList();
+  },
+  methods: {
+    logout() {
+      window.sessionStorage.removeItem("token");
+      this.$router.push("./login");
     },
-    
-}
+    async getMenuList() {
+      const { data: res } = await this.$http.get("menus");
+      if (res.meta.status != 200) return this.$message.error(res.meta.status);
+      this.menuList = res.data;
+      console.log(res);
+    },
+  },
+};
 </script>
 
 
 <style lang="less" scoped>
-    
+.home-container {
+  height: 100%;
+}
+.el-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #373d41;
+  padding-left: 0;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
+
+
+i.iconfont {
+  padding-right: 10px;
+}
+
 </style>
